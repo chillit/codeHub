8f6aa811fc9ac45ca8d74f1cfc3d024c4c57f468
Akanov-Kanta <129672907+Akanov-Kanta@users.noreply.github.com>
2023-09-24 14:25:56 +0600

    home(NOT ALL

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:duolingo/src/home/login/login_page.dart';
 import 'package:duolingo/src/home/main_screen/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:duolingo/src/utils/firebase_api.dart';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +11,7 @@ import 'src/home/main_screen/home.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await FirebaseApi().initNotifications();
+
   runApp(MyApp());
 }
 
